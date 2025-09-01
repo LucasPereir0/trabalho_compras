bd7ba7e88bfbd93910380b77e4c152bffe1e6874
LUCAS PEREIRA <lucas_pereira23@estudante.sesisenai.org.br>
2025-09-01 09:18:40 -0300

    Trabalho Carlos

---
+++ controller/compra.controller.js
@@ -0,0 +1,78 @@
+const Compra = require('../model/Compra')
+
+const cadastrar = async (req,res)=>{
+    const valores = req.body
+    try{
+        const dados = await Compra.create(valores)
+        res.status(200).json(dados)
+    }catch(err){
+        console.error('Erro ao cadastrar os dados!',err)
+        res.status(500).json({message: 'Erro ao cadastrar os dados!'})
+    }
+}
+
+const listar = async (req,res)=>{
+    try{
+        const dados = await Compra.findAll()
+        res.status(200).json(dados)
+    }catch(err){
+        console.error('Erro ao listar os dados!',err)
+        res.status(500).json({message: 'Erro ao listar os dados!'})
+    }
+}
+
+
+const apagar = async (req,res)=>{
+    const idCompra = req.params.id
+    try{
+        const dados = await Compra.findByPk(idCompra)
+        if(dados){
+            await Compra.destroy({where: { idCompra: idCompra}})
+            res.status(204).json({message: 'Dados excluídos com sucesso!'})
+        }else{
+            res.status(404).json({message: 'Compra não encontrado!'})
+        }    
+    }catch(err){
+        console.error('Erro ao apagar os dados!',err)
+        res.status(500).json({message: 'Erro ao apagar os dados!'})
+    }
+}
+
+
+const atualizar = async (req,res)=>{
+    const idCompra = req.params.id
+    const valores = req.body
+    try{
+        let dados = await Compra.findByPk(idCompra)
+        if(dados){
+            await Compra.update(valores, {where: { idCompra: idCompra}})
+            dados = await Compra.findByPk(idCompra)
+            res.status(200).json(dados)
+        }else{
+            res.status(404).json({message: 'Compra não encontrado!'})
+        }
+    }catch(err){
+        console.error('Erro ao atualizar os dados!',err)
+        res.status(500).json({message: 'Erro ao atualizar os dados!'})
+    }
+}
+
+const listarId = async (req,res)=>{
+    const valor = req.params.id
+    console.log(valor)
+    try{
+        const dados = await Compra.findOne({where: {idCompra: valor}})
+        if(dados === null){
+            console.log('Compra não encontrado!')
+            res.status(404).json({message: 'Compra não encontrado!'})
+        }else{
+            console.log(dados)
+            res.status(200).json(dados)
+        }
+    }catch(err){
+        console.error(`Erro ao listar dados do gráfico!`,err)
+        res.status(500).json({message: 'Erro ao listar dados do gráfico!'})
+    }
+}
+
+module.exports = { cadastrar, listar, apagar, atualizar, listarId }
